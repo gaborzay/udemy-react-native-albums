60ce4e8a11ac76665e256f928d7ef76461354401
gabor <GaborZay@gmail.com>
2018-04-08 23:02:02 -0700

    Added in Hello world to index

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+//import App from './App';
 
+// Create a component
+const App = () => (
+    <Text>Hello World!</Text>
+);
+
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
