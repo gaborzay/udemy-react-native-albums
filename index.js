3a2eb4397d2b3efb718159381e5e126ce5bdcf95
gabor <GaborZay@gmail.com>
2018-04-09 19:40:44 -0700

    Adding header and centering

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // Import a library to help create a component
-import React from 'react';
-import { AppRegistry, Text } from 'react-native';
-//import App from './App';
-
-// Create a component
-const App = () => (
-    <Text>Hello World!</Text>
-);
+import { AppRegistry } from 'react-native';
+import App from './App';
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
